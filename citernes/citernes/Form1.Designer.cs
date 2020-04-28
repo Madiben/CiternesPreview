2f185016ad75a512ceaa22f7c224a114c9e45daf
Bentaleb Mahdi <44513501+Madiben@users.noreply.github.com>
2020-04-28 01:54:45 +0100

    Add files via upload

---
+++ citernes/citernes/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace citernes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelvolume = new System.Windows.Forms.Label();
+            this.labeltaux = new System.Windows.Forms.Label();
+            this.textBoxQuan = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Button();
+            this.mini = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(310, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(468, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Retirer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(626, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Vider";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Capacité";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Quantité +-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Volume coutant:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(233, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Taux de remplissage";
+            // 
+            // labelvolume
+            // 
+            this.labelvolume.AutoSize = true;
+            this.labelvolume.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelvolume.Location = new System.Drawing.Point(330, 269);
+            this.labelvolume.Name = "labelvolume";
+            this.labelvolume.Size = new System.Drawing.Size(43, 23);
+            this.labelvolume.TabIndex = 7;
+            this.labelvolume.Text = "0 L";
+            // 
+            // labeltaux
+            // 
+            this.labeltaux.AutoSize = true;
+            this.labeltaux.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltaux.Location = new System.Drawing.Point(330, 321);
+            this.labeltaux.Name = "labeltaux";
+            this.labeltaux.Size = new System.Drawing.Size(76, 23);
+            this.labeltaux.TabIndex = 8;
+            this.labeltaux.Text = "0.0 %";
+            // 
+            // textBoxQuan
+            // 
+            this.textBoxQuan.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxQuan.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxQuan.Location = new System.Drawing.Point(310, 137);
+            this.textBoxQuan.Name = "textBoxQuan";
+            this.textBoxQuan.Size = new System.Drawing.Size(468, 24);
+            this.textBoxQuan.TabIndex = 11;
+            this.textBoxQuan.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBoxQuan.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(310, 50);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 27);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "2000 L";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(450, 50);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 27);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "3000 L";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(310, 369);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(468, 23);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 428);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Tp Developer par Bentaleb Mahdi ";
+            // 
+            // exit
+            // 
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.Image = ((System.Drawing.Image)(resources.GetObject("exit.Image")));
+            this.exit.Location = new System.Drawing.Point(758, 0);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(43, 42);
+            this.exit.TabIndex = 16;
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // mini
+            // 
+            this.mini.FlatAppearance.BorderSize = 0;
+            this.mini.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mini.Image = ((System.Drawing.Image)(resources.GetObject("mini.Image")));
+            this.mini.Location = new System.Drawing.Point(709, 0);
+            this.mini.Name = "mini";
+            this.mini.Size = new System.Drawing.Size(43, 42);
+            this.mini.TabIndex = 17;
+            this.mini.UseVisualStyleBackColor = true;
+            this.mini.Click += new System.EventHandler(this.mini_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mini);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBoxQuan);
+            this.Controls.Add(this.labeltaux);
+            this.Controls.Add(this.labelvolume);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelvolume;
+        private System.Windows.Forms.Label labeltaux;
+        private System.Windows.Forms.TextBox textBoxQuan;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button mini;
+    }
+}
+
---
+++ citernes/citernes/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace citernes
+{
+    public partial class Form1 : Form
+    {
+        float taux = 0, tx = 0;
+        int vol=0,en=0;
+        int cap=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Voulez vous vraiment vider la citerne", "Alert", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                textBoxQuan.Text = "";
+                labelvolume.Text = "0 L";
+                labeltaux.Text = "0.0 %";
+                progressBar1.Value = 0;
+
+            }
+            else
+            {
+            }
+            
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mini_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxQuan.Text == "")
+            {
+                MessageBox.Show("Il faut saisir une valeur : ", "Erreur de saisi" , MessageBoxButtons.OK);
+            }
+            else
+            {
+                if(radioButton1.Checked==true)
+                {
+                    cap = 2000;
+                    if (taux != 100)
+                    {
+                        vol = vol + Convert.ToInt32(textBoxQuan.Text);
+                        if (vol <= cap)
+                        {
+                            en = vol;
+                            labelvolume.Text = Convert.ToString(vol) + "L";
+                            taux = vol * 100 / cap;
+                            
+                            if (taux <= 100)
+                            {
+                                tx = taux;
+                                labeltaux.Text = taux + "%";
+                                progressBar1.Value = Convert.ToInt32(taux);
+                            }else
+                            {
+                                labeltaux.Text = Convert.ToDouble(tx) + "%";
+                                progressBar1.Value = Convert.ToInt32(tx);
+                            }
+                        }
+                        else
+                        {
+                            vol = en;
+                            MessageBox.Show("la citerne est Contient Seul " + cap + " L Si Vous Plait Entrer Un Valeur Acceptable: ", "Erreur de saisi", MessageBoxButtons.OK);
+                        }
+                    }
+                }
+                else
+                {
+
+                    cap = 3000;
+                    
+                        vol = vol + Convert.ToInt32(textBoxQuan.Text);
+                        if (vol <= cap)
+                        {
+                            en = vol;
+                            labelvolume.Text = Convert.ToString(vol) + "L";
+                            taux = vol * 100 / cap;
+                            if (taux <= 100)
+                            {
+                                tx = taux;
+                                labeltaux.Text = taux + "%";
+                                progressBar1.Value = Convert.ToInt32(taux);
+                            }
+                            else
+                            {
+                                labeltaux.Text = Convert.ToString(tx) + "%";
+                                progressBar1.Value = Convert.ToInt32(tx);
+                            }
+                        }
+                        else
+                        {
+                            vol =en;
+                            MessageBox.Show("la citerne est Contient Seul " + cap + " L Si Vous Plait Entrer Un Valeur Acceptable: ", "Erreur de saisi", MessageBoxButtons.OK);
+                        }
+                    
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBoxQuan.Text == "")
+            {
+                MessageBox.Show("Il faut saisir une valeur : ", "Erreur de saisi", MessageBoxButtons.OK);
+            }
+            else
+            {
+                if (radioButton1.Checked == true)
+                {
+                    cap = 2000;
+
+                    if (vol <= cap)
+                    {
+                        vol = vol - Convert.ToInt32(textBoxQuan.Text);
+                        if (vol > 0)
+                        {
+                            labelvolume.Text = Convert.ToString(vol) + "L";
+                            taux = vol * 100 / cap;
+                            if (taux > 0)
+                            {
+                                labeltaux.Text = Convert.ToString(taux) + "%";
+                                progressBar1.Value = Convert.ToInt32(taux);
+                            }
+                            else
+                            {
+                                labeltaux.Text = Convert.ToString(0) + "%";
+                                progressBar1.Value = Convert.ToInt32(0);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("la citerne est Contient Seul " + cap + " L Si Vous Plait Entrer Un Valeur Acceptable: ", "Erreur de saisi", MessageBoxButtons.OK);
+                    }
+                }
+                else
+                {
+                    cap = 3000;
+
+                    if (vol <= cap)
+                    {
+                        vol = vol - Convert.ToInt32(textBoxQuan.Text);
+                        if (vol > 0)
+                        {
+                            labelvolume.Text = Convert.ToString(vol) + "L";
+                            taux = vol * 100 / cap;
+                            if (taux > 0)
+                            {
+                                labeltaux.Text = Convert.ToString(taux) + "%";
+                                progressBar1.Value = Convert.ToInt32(taux);
+                            }
+                            else
+                            {
+                                labeltaux.Text = Convert.ToString(0) + "%";
+                                progressBar1.Value = Convert.ToInt32(0);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("la citerne est Contient Seul " + cap + " L Si Vous Plait Entrer Un Valeur Acceptable: ", "Erreur de saisi", MessageBoxButtons.OK);
+                    }
+
+                }
+            }
+        }
+    }
+}
